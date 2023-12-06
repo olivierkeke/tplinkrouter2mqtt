0fa8e83461597208444a67d75733b6a60b839e33
Olivier Marceau <hollysaiqs@marceau.ovh>
2023-12-06 22:11:01 +0100

    Add device description

--- tplinkrouter/mqtt.py
+++ tplinkrouter/mqtt.py
@@ -19,7 +19,9 @@ class MQTTCommunicator:
     async def publish_state(self):
         while True:
             wifi_state = await self.state_message_queue.get()
-            await self.client.publish("tplinkrouter/wifi", payload=json.dumps(wifi_state))
+            payload = json.dumps(wifi_state)
+            await self.client.publish("tplinkrouter/wifi", payload=payload)
+            logging.debug(f'sent message to mqtt with payload: {payload}')
 
     async def listen_to_command(self):
         async with self.client.messages() as messages:
@@ -38,11 +40,19 @@ class MQTTCommunicator:
             "unique_id": "wifi_switch",
             "value_template": "{{ value_json.Status }}",
             "payload_on": "Up",
-            "payload_off": "Disable"
+            "payload_off": "Disabled",
+            "icon": "mdi:wifi",
+            "device": {
+                "identifiers": [
+                    "TD-W870"
+                ],
+                "manufacturer": "TPlink"
+            }
         }
         await self.client.publish(
             f"{self.discovery_prefix}/switch/tplinkrouter/config",
             payload=json.dumps(hass_discovery_switch),
             retain=True
         )
+        logging.info(f"hass discovery config sent: {hass_discovery_switch}")
 
