e17a7c2f73bc4e3148aba636bea89d3b84ae2dd1
Olivier Marceau <hollysaiqs@marceau.ovh>
2023-12-18 14:33:38 +0100

    Add qss switch

--- tplinkrouter/mqtt.py
+++ tplinkrouter/mqtt.py
@@ -8,7 +8,7 @@ from typing import Dict
 import aiomqtt
 
 from .telnet import TelnetCommunicator
-from .tplink_config import WIFI_ON_CMD, WIFI_OFF_CMD
+from .tplink_config import WIFI_ON_CMD, WIFI_OFF_CMD, QSS_ON_CMD, QSS_OFF_CMD
 
 
 @dataclass
@@ -36,6 +36,14 @@ class MQTTCommunicator:
                     logging.warning('Command message queue is full')
 
     async def hass_discovery(self):
+        device = {
+            "name": f'TPLink {self.telnet_communicator.serial}',
+            "model": f'tplink_{self.telnet_communicator.serial}',
+            "identifiers": [
+                self.telnet_communicator.serial
+            ],
+            "manufacturer": "TPlink"
+        }
         hass_discovery_switch = {
             "name": "wifi",
             "state_topic": "tplinkrouter/wifi",
@@ -47,18 +55,29 @@ class MQTTCommunicator:
             "payload_on": WIFI_ON_CMD,
             "payload_off": WIFI_OFF_CMD,
             "icon": "mdi:wifi",
-            "device": {
-                "name": f'TPLink {self.telnet_communicator.serial}',
-                "model": f'tplink_{self.telnet_communicator.serial}',
-                "identifiers": [
-                    self.telnet_communicator.serial
-                ],
-                "manufacturer": "TPlink"
-            }
+            "device": device
         }
         await self.client.publish(
-            f"{self.discovery_prefix}/switch/tplinkrouter/config",
+            f"{self.discovery_prefix}/switch/{device['model']}/config",
             payload=json.dumps(hass_discovery_switch),
+            retain=True
+        )
+        hass_discovery_qss = {
+            "name": "QSS",
+            "state_topic": "tplinkrouter/wifi",
+            "command_topic": "tplinkrouter/wifi/set",
+            "unique_id": "wifi_switch",
+            "value_template": "{{ value_json.QSS }}",
+            "state_on": "Enable",
+            "state_off": "Disabled",
+            "payload_on": QSS_ON_CMD,
+            "payload_off": QSS_OFF_CMD,
+            "icon": "mdi:wifi",
+            "device": device
+        }
+        await self.client.publish(
+            f"{self.discovery_prefix}/switch/{device['model']}/config",
+            payload=json.dumps(hass_discovery_qss),
             retain=True
         )
         logging.info(f"hass discovery config sent: {hass_discovery_switch}")
