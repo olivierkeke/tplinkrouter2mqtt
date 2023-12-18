9027f5a55afbf521cd2f676bd3a2957ba365434c
Olivier Marceau <hollysaiqs@marceau.ovh>
2023-12-18 14:10:47 +0100

    Add log_level option

--- tplinkrouter/main.py
+++ tplinkrouter/main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 from argparse import ArgumentParser
+from typing import Union
 
 import aiomqtt
 
@@ -11,6 +12,14 @@ from tplinkrouter.telnet import TelnetCommunicator
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
+
+def parse_log(log_level: Union[str, int]) -> int:
+    if isinstance(log_level, int):
+        return log_level
+    elif isinstance(log_level, str):
+        return eval(f'logging.{log_level}')
+    else:
+        raise ValueError(f'{log_level} is not a valid log level')
 
 async def launch():
     parser = ArgumentParser(
@@ -25,8 +34,13 @@ async def launch():
     parser.add_argument('--mqtt_port', type=int, default=os.environ.get("MQTT_PORT"))
     parser.add_argument('--mqtt_username', default=os.environ.get("MQTT_USERNAME"))
     parser.add_argument('--mqtt_password', default=os.environ.get("MQTT_PASSWORD"))
+    parser.add_argument('--log_level', type=parse_log,
+                        default=os.environ.get("LOG_LEVEL"),
+                        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'])
 
     args = parser.parse_args()
+    if args.log_level:
+        logging.basicConfig(level=args.log_level)
     logging.info(f'Connecting to mqtt broker {args.mqtt_host}')
     telnet_communicator = TelnetCommunicator(
         username=args.tplink_username,
