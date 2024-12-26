ee65edde9ce549420a6a4c7103eaf6c7653eace4
Olivier Marceau <hollysaiqs@marceau.ovh>
2024-12-26 12:19:09 +0100

    Initialize aiomqtt context manager

--- tplinkrouter/main.py
+++ tplinkrouter/main.py
@@ -63,10 +63,11 @@ async def launch():
     while True:
         try:
             async with telnet_communicator:
-                await asyncio.gather(
-                    mqtt_communicator.publish_state(),
-                    mqtt_communicator.listen_to_command(),
-                )
+                async with client:
+                    await asyncio.gather(
+                        mqtt_communicator.publish_state(),
+                        mqtt_communicator.listen_to_command(),
+                    )
         except:
             print(f"Connection to telnet server lost; Reconnecting in {interval} seconds ...")
             await asyncio.sleep(interval)
