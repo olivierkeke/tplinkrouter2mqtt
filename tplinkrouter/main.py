c36b31099929b59b9c4187c6035655b628c694f5
Olivier <olivierkeke@users.noreply.github.com>
2025-01-04 11:54:48 +0100

    Fix previous PR mess up (#5)
    * Use a context manager
    
    * Fix typo
    
    * Adapt to aiomqtt API changes
    
    * Initialize aiomqtt context manager
    
    * Fix context manager close method args
    
    * Améliore logging
    
    * Fix for MQTT communicator
    
    * Replace deprecated logging.warn() method
    
    * Initialize mqtt client resource manager before publishing
    
    * Only one with for mqtt client
    
    * Make MQTTCommunicator a context resource
    
    * await corountine

--- tplinkrouter/main.py
+++ tplinkrouter/main.py
@@ -64,15 +64,14 @@ async def launch():
         try:
             async with telnet_communicator:
                 logging.info("launching MQTT listening and publishing task")
-                await mqtt_communicator.send_hass_discovery()
-                logging.info("HASS discovery message sent")
-                await asyncio.gather(
-                    mqtt_communicator.publish_state(),
-                    mqtt_communicator.listen_to_command(),
-                )
+                async with mqtt_communicator:
+                    await asyncio.gather(
+                        mqtt_communicator.publish_state(),
+                        mqtt_communicator.listen_to_command(),
+                    )
                 logging.info("MQTT listening and publishing task terminated")
         except:
-            logging.warning("Connection to telnet server lost; Reconnecting in %i seconds ...", interval)
+            logging.warning("Connection to telnet or mqtt server lost; Reconnecting in %i seconds ...", interval)
             await asyncio.sleep(interval)
 
 
